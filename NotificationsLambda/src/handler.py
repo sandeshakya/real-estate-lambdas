241144e6d111810827164872b0c55832c444e98b
Sandesh Shakya <sandeshakya94@gmail.com>
2021-03-28 02:38:39 -0600

    notification lambda

--- NotificationsLambda/src/handler.py
+++ NotificationsLambda/src/handler.py
@@ -1,16 +1,40 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 import os
+import json
 from datetime import datetime, timedelta
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.environ.get("TABLE_NAME"))
 
-def handler(event,context):
-    resp = table.query(
-        Select='ALL_ATTRIBUTES',
-        KeyConditionExpression=Key('timestamp').gte(round(datetime.timestamp(datetime.now() - timedelta(hours=1))*1000))
-    )
-    print(resp)
+sns = boto3.resource('sns')
+topic = sns.Topic(os.environ.get('SNS_TOPIC'))
+listing_types = ['POINT2HOMES', 'SASKHOUSES']
+point2home_baseurl = 'https://www.point2homes.com'
 
-    return {'status':200}
+
+def handler(event, context):
+    links = []
+    for listing_type in listing_types:
+
+        resp = table.query(
+            IndexName='type-createdat-index',
+            KeyConditionExpression=Key('type').eq(listing_type) & Key('createdat').gte(
+                round(datetime.timestamp(datetime.now() - timedelta(minutes=5))*1000))
+
+        )
+        for item in resp['Items']:
+            links.append(item['link'] if listing_type ==
+                         'SASKHOUSES' else point2home_baseurl + item['link'])
+    print('Found {} new listings'.format(len(links)))
+
+    topic.publish(
+        MessageStructure='json',
+        Subject='Found new Listings',
+        Message=json.dumps({
+            'default': 'Found {} new listings. Check your email'.format(len(links)),
+            'email': "Found these {} new listings!\n\n{}".format(len(links), '\n'.join(links))
+        })
+    )
+    print('Messages sent')
+    return {'status': 200}
